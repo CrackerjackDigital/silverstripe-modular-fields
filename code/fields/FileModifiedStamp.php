54710557d2f8063c92cd021b8674cf6a6f8690ec
Steve Wake <steve@moveforward.co.nz>
2017-05-15 13:13:00 +1200

    pagination and search options

--- code/fields/FileModifiedStamp.php
+++ code/fields/FileModifiedStamp.php
@@ -1,15 +1,24 @@
 <?php
-
 namespace Modular\Fields;
 
+/**
+ * Add to a File to track the last modified time (in unix time) of a file e.g. from filemtime() call.
+ */
+
 use Modular\Traits\file_changed;
+use Modular\Traits\md5;
 use Modular\TypedField;
 use Modular\Types\IntType;
 
 class FileModifiedStamp extends TypedField implements IntType {
 	use file_changed;
+	use md5;            // for hash_file
 
 	const Name = 'FileModifiedStamp';
+
+	public function hashFile($fileName = '') {
+		return static::hash_file( $fileName ?: $this()->Filename);
+	}
 
 	public function onBeforeWrite() {
 		parent::onBeforeWrite();
