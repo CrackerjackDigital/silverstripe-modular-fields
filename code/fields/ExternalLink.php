8c88a75f2be75999aaa5aeb71690316d29d10060
Steve Wake <steve@moveforward.co.nz>
2017-09-18 22:44:43 +1200

    work from delectus-demo

--- code/fields/ExternalLink.php
+++ code/fields/ExternalLink.php
@@ -1,27 +1,29 @@
 <?php
+
 namespace Modular\Fields;
 
-use Modular\Field;
+use Modular\TypedField;
 use Modular\Types\URLType;
 
 class ExternalLink extends TypedField implements URLType {
-	const ExternalLinkOption    = 'ExternalLink';
-	const Name       = 'ExternalLink';
-	// const Schema     = 'Text';
+	const ExternalLinkOption = 'ExternalLink';
+	const Name               = 'ExternalLink';
 
 	/**
 	 * If value does not have a schema then prefix 'file://'
+	 *
 	 * @param null $typeCast ignored
+	 *
 	 * @return mixed|string
 	 * @throws \Modular\Exceptions\Exception
 	 */
-	public function typedValue($typeCast = null) {
+	public function typedValue( $typeCast = null ) {
 		$value = $this->singleFieldValue();
-		if (false == strpos($value, '://')) {
+		if ( false == strpos( $value, '://' ) ) {
 			$value = static::TypedValuePrefix . $value;
 		}
+
 		return $value;
 	}
-
 
 }
